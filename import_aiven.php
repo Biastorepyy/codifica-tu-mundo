b4d1d3d81a0dff2f48d98967e998601ebc923d1a
Biastorepyy <nanigale12@gmail.com>
2026-07-08 10:30:39 -0300

    Add files via upload

--- import_aiven.php
+++ import_aiven.php
@@ -1,6 +1,111 @@
 <?php
 require_once __DIR__ . '/includes/db.php';
 
+$list_5a = "Achucarro Cuba, Icker Rodrigo
+Acosta Arguello, Gabriela María Luján
+Aguilera Velázquez, Meliza Soeli
+Alvarenga Cespedes, Marcos Misael
+Alvarez Britez, Amalia Cibelle
+Amarilla Román, Matheo Samuel
+Aranda González, Ruth Fabiola
+Báez Velázquez, Pablo Josué
+Bareiro Brizuela, Michelli Jazmín
+Bavera Enciso, Aaron Jesús
+Benítez García, Dylan Matías
+Britez Gamarra, Lucía Milagro
+Cáceres Cabrera, Melany Aryane
+Cáceres Candia, Sofía Luján
+Cano Ledesma, Cristel Milena
+Coronel Britos, Mía Valentina
+Díaz Fernández, Cesar Iam
+Duarte Duarte, Thiago Joaquín
+Escobar Torres, Isaías Santiago
+Espínola Espínola, Thiago Josue
+Estigarribia González, Katherine Monserrath
+Ferreira Ávalos, Silvia Marisol
+Ferreira Castro, Sofía Mariel
+Figueredo Báez, Josías Miguel
+Flores Zapattini, Paloma Nicolle
+Franco Aquino, Milagros Elizabeth
+Frutos Sanabria, Victor Aaron
+Gamarra Fariña, Lucas Mateo
+Giménez Espínola, Samira Nicole
+Gimenez García, Jeremías Andrés
+Godoy Aguirre, Axel Ariel
+Gómez Insaurralde, Jorge Leonardo
+Herrera Idoyaga, Alexander de Jesús
+Jara Da Silva, Julieta María Saharí
+Larroza Garay, Guillermo Gael
+Lime Davalos, Sofía Rebeca
+Lisboa Ledesma, Juan David
+Maciel Ozuna, David Luiz
+Melgarejo Pereira, Dylan Santiago
+Molas Marecos, Max Missael
+Morgalos Martínez, Kael Abraham
+Morán Sánchez, Kevin Fabián
+Ojeda Giménez, Gabriel Josue
+Paredes Rivas, Lucas Paul
+Reguera Benítez, Edgar Elías
+Rodríguez Cabrera, Cesar Asmir
+Rojas Cardozo, Abigail Yeruti
+Rotela Nuñez, Hector Josue
+Silvero Benítez, Zoe Abigaíl
+Talavera Martínez, Alaia Jazmín
+Vera Franco, Izán Israel";
+
+$list_5b = "Acosta Benitez, Victoria
+ALONZO MONTEIRO DE ARAUJO, LAURA ESTHER
+Alvarenga Fretes, Oliver Damian
+Avalos Britez, Didier Daniel
+Balmaceda Ferreira, Fernando Josue
+Basaldua Escurra, Liam Gael
+Benitez Escurra, Kiara Lujan
+Britez Fernandez Sol Fiorella
+Brítos Reyes, Araceli Luján
+Caballero Gomez, Maria Sol Arami
+Cantero Alderete, Thiago Emmanuel
+Cardenas Zarate, Thiago Gael
+Castellano Vera, Thiago Nahuel
+Cristaldo Galindo, Angel Emanuel
+Cristaldo Jara, Emma Victoria
+Denis Leiva, Santiago Gabriel
+Doldán Román, Santino Diosnel
+Escurra Paredes, Elvio Jeús
+Fernandez Vazquez, Danna Alejandra
+Galeano García, Mia Nahiara
+Galeano Samudio, Gianna Antonella
+Garcia Avalos, Jesus Antonio
+García Navarro, Arely Luana
+Gimenez Herrera, Genesis Abigail
+Gimenez Mendez, Sally Victoria
+Gomez Davalos, Antonella Abigail
+Gonzalez Amarilla, Leidy Carolina
+Gonzalez Zarate, Josías Samuel
+Ibañez Leguizamón, Franco Miguel
+Jara Da Silva, Lucero Aurora
+Lauter Montiel, Charlotte Pietra
+López López, Roberto Daniel
+López Quiñónez, Cristhofer Daniel
+Lopez Rolon, Aylla Nahiara
+Martinez Gauto, Katheryn Thais
+Meza Marzal, Enzo Benjamin
+Molinas Segovia, Erika Lujan
+Moreno Martínez, Hugo Ariel
+Ocampos Sala, Mia Belén
+Ojeda Gimenez, Marthin Gael
+Paez Irala, Josias Serafin
+Pereira Acevedo, Sofia Micaela
+Rodriguez Ibarra, Bernardo Abel
+Rojas Ortellado, Mariana Ayleen
+Rolon Irala, Fabiani Ezequiel
+Salinas Aguirre, Zamira Lujan
+Sandoval Ramirez, Aldo Andres
+Silvero Ruiz Díaz, Johan Daniel
+Tellez Elizeche, Oliver Dadiel
+Valderrama López, Gabriel
+Velazco Velazquez, Eduardo Nicolás
+Vera González, Zoe Magali";
+
 $list_5c = "Alcaraz Passerini, Danna Maite
 Acosta Benítez, Brenda Abigail
 Aguilar Fleitas, Benjamín
@@ -85,6 +190,111 @@ Vargas Martínez, Sofia Clarisse
 Velazquez Gallardo, Luana Araceli
 Verza Oviedo, Vitor Manuel
 Zaracho Fernández, Marian Gissel";
+
+$list_6a = "Acosta Florentin, Angie Gabriela
+Adorno, Alejandro Miguel
+Alarcón Paredes, Igor Nahuel
+Ávalos Fretes, Thais Aylen
+Báez Cáceres, Perla Celeste
+Báez Noguera, Paloma Janice
+Báez Rivas, Nahiara Anabel
+Bazán Bareiro, Alberto Junior
+Bazán González, Mathias Gael
+Benítez Britez, Thiago Alexander
+Benítez Núñez, Alejandro Nahuel
+Benítez Paredes, Yanina Itzel
+Bogado Dávalos, Luján Jassiel
+Bogarín Rodríguez, Selena Madeleine
+Britez Acosta, Sofía Guadalupe
+Cabrera Duarte, Jesús Emanuel
+Cáceres Duarte, Alan Sebastian
+Cohene Sanchez, Aynara Abigail
+Cuba Molinas, Fabio Alexander
+Duarte Cabrera, Thiago Jesús
+Duarte Vera, Sofía Isabella
+Escobar Duarte, Adriana Isabela
+Espínola Gomez, Fernando Samuel
+Fariña Martínez, Amira Isabella
+Ferreira González, Lucas David
+Franco Martínez, Helem Beatriz
+Gamarra Pereira, Valery Gabriela
+Gomez Durañona, Santiago Benjamín
+Gómez Fernández, Carla Nazareth
+González Medina, Ever Sebastian
+Idoyaga Ríos, Aranza Abigail
+Irala Correa, Jonás Emanuel
+Martínez Rojas, Sofía Anahí
+Melgarejo Samudio, Melissa Mariel
+Pico Lambaré, Michel Alexander
+Pintos Enciso, Zahira Gabriela
+Portillo Morfín, Elías Daniel
+Ramírez Torres, Rocío Aramí
+Roa Castillo, Fabio Rafael
+Rojas Benitez, Magalí Abigaíl
+Rolón Dure, Mónica María Paz
+Romero González, Danna Fiorella
+Sánchez Muñiz, Pedro Fernando
+Segovia Sánchez, Kathia Belinda Aide
+Sosa Mendoza, Santiago Nicolás
+Vera Villalba, Dahiana Aylen
+Villagra Vera, Vanía Abigaíl
+Villalba Benítez, Lucas Julián
+Villalba Holt, Mathias Benjamín";
+
+$list_6b = "Aguirre Cabrera, Hugo David
+Alfonzo Duarte, Facundo Joaquin
+Alviso Céspedes, Milagros Milena
+Aranda González, Armando Daniel
+Ayala Cárdenas, Erica Mariana
+Benítez Mancia, Veronica Abigail
+Benítez Sánchez, Fide Alexander
+Bobadilla Coronel, Bruno Manuel
+Bogado Balbuena, Iván Tadeo
+Britez Melgarejo, Thiago Nahuel
+Cano Sánchez, Matías Benjamín
+Cardozo Soto, Luciana Magalí
+Castillo Armoa, Micaela Rubí
+Colman Mora, Alisson Gabriela
+Fernandez Vera, Alejandro Aquiles
+Ferreira Bogado, Mirella Aylén
+Franco Fleitas, Isabela Jeruti
+Frutos Sanabria, Victor Daniel
+Giménez Ayala, Sofia Isabella
+Giménez Iglesias, Suri Nicol
+Giménez Vera, Samuel Edgar
+González Almada, Derlis Gustavo Emmanuel
+González Espínola, Josías Daniel
+Hermosilla Vera, Danna Abigail
+Leguizamón Martínez, Julieta Maite
+López Arevalos, Mathias Emanuel
+Martínez González, Milena Abigail
+Martínez Nuñez, Katherin Jazmin
+Medina Torres, Elias Josué
+Morán Sánchez, Sofia Magdalena
+Ojeda Gonzalez, Paz Thainara
+Ojeda Pereira, Danna Nicole
+Olmedo Morinigo, Vanina Guadalupe
+Ovelar Nuñez, Lucas Tadeo
+Pérez Alvarenga, Dylan Josué
+Pires González, Murilo Vinicius
+Prieto Achucarro, Dylhan David
+Prieto Sostoa, Thiago Nahuel
+Recalde Aquino, Sofia Isabel
+Recalde Rios, Arianne Valentina
+Rojas Cardozo, Anahi Paloma
+Rojas Méndez, Sofia Milagros
+Sosa Báez, Melanie Licebt
+Toledo Ayala, Alex Daniel
+Toledo Mora, Victoria Alejandra
+Torales González, Isaías Eduardo
+Trinidad Rojas, Danna Valentina
+Uldera Ramos, Genesis Rocio
+Valdez Ibarra, Marcelo Alejandro
+Vega Godoy, Judy Fiomara
+Vera Frutos, Kevin Ivan
+Villar González, Luis Fernando
+Vázquez Ramírez, Raissa Janire
+Zarza Rivas, Alvaro David";
 
 $list_6c = "Aguilera Fariña, Daniela Alexandra
 Aguilar Grance, Robin Giovani
@@ -180,8 +390,12 @@ Yegros Fretes, Kenny Esteban
 Zapata Rojas, César Isaac";
 
 $lists = [
+    ['grado' => 1, 'seccion' => 'A', 'data' => explode("\n", $list_5a)],
+    ['grado' => 1, 'seccion' => 'B', 'data' => explode("\n", $list_5b)],
     ['grado' => 1, 'seccion' => 'C', 'data' => explode("\n", $list_5c)],
     ['grado' => 1, 'seccion' => 'D', 'data' => explode("\n", $list_5d)],
+    ['grado' => 2, 'seccion' => 'A', 'data' => explode("\n", $list_6a)],
+    ['grado' => 2, 'seccion' => 'B', 'data' => explode("\n", $list_6b)],
     ['grado' => 2, 'seccion' => 'C', 'data' => explode("\n", $list_6c)],
     ['grado' => 2, 'seccion' => 'D', 'data' => explode("\n", $list_6d)],
 ];
@@ -214,9 +428,10 @@ foreach ($lists as $list) {
     foreach ($list['data'] as $name) {
         $name = trim($name);
         if (!empty($name)) {
+            $nameFormateado = mb_convert_case(strtolower($name), MB_CASE_TITLE, "UTF-8");
             $data['estudiantes'][] = [
                 'id' => $nextId++,
-                'nombre' => $name,
+                'nombre' => $nameFormateado,
                 'grado' => $g,
                 'seccion' => $s
             ];
@@ -261,7 +476,8 @@ try {
         foreach ($list['data'] as $name) {
             $name = trim($name);
             if (!empty($name)) {
-                $stmt->execute([$name, $g, $s]);
+                $nameFormateado = mb_convert_case(strtolower($name), MB_CASE_TITLE, "UTF-8");
+                $stmt->execute([$nameFormateado, $g, $s]);
                 $count++;
             }
         }
@@ -303,7 +519,8 @@ try {
         foreach ($list['data'] as $name) {
             $name = trim($name);
             if (!empty($name)) {
-                $stmt->execute([$name, $g, $s]);
+                $nameFormateado = mb_convert_case(strtolower($name), MB_CASE_TITLE, "UTF-8");
+                $stmt->execute([$nameFormateado, $g, $s]);
                 $count++;
             }
         }
